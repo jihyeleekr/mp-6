5a32b98a22b022111a4fd97f55658bc9ac60d896
Jihye Lee <jh020211@bu.edu>
2025-04-27 20:39:46 -0400

    error fixed

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.LINKEDIN_CLIENT_ID}&redirect_uri=${process.env.NEXT_BASE_URL}/api/auth/callback&scope=openid%20profile%20email`;
-    console.log("Login URL:", authUrl); // add this to see full URL
+    const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.LINKEDIN_CLIENT_ID}&redirect_uri=${process.env.LINKEDIN_REDIRECT_URI}&scope=openid%20profile%20email`;
     return NextResponse.redirect(authUrl);
 }
